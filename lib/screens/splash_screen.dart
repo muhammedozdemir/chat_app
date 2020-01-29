5ef33e85d84e793641e8b27b3bf0fb4c0bac6c71
Ray Li <ray.li.verified@gmail.com>
2020-01-29 17:31:02 -0600

    Build Updates

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,9 @@
 import 'package:chat_app/controllers/user_controller.dart';
 import 'package:chat_app/widgets/login_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:onesignal/onesignal.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,25 +16,29 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.white,statusBarBrightness: Brightness.dark));
-    UserController.isLoggedIn().then((val){
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+        statusBarColor: Colors.white, statusBarBrightness: Brightness.dark));
+    UserController.isLoggedIn().then((val) {
       setState(() {
-        isLoggedIn=val;
+        isLoggedIn = val;
       });
-      if(isLoggedIn){
+      if (isLoggedIn) {
         Navigator.of(context).pushNamed('/main');
       }
     });
-    WidgetsBinding.instance.addObserver(LifecycleEventHandler(resumeCallBack: ()async{
+    WidgetsBinding.instance
+        .addObserver(LifecycleEventHandler(resumeCallBack: () async {
       print("Resume");
       UserController.updateUser(true);
-    },suspendingCallBack: ()async{
+    }, suspendingCallBack: () async {
       print("Stopped");
       UserController.updateUser(false);
     }));
     OneSignal.shared.init("48eaffb3-f899-4b01-8de5-c60029af39dc");
-    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
   }
+
   @override
   Widget build(BuildContext context) {
     double imageSize = MediaQuery.of(context).size.width / 3;
@@ -48,32 +52,47 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            isLoggedIn==null?CircularProgressIndicator():!isLoggedIn?LoginButton("Continue with Facebook",
-                color: Color(0xff3b5998),
-                onPressed: initiateFacebookLogin,
-                leading: Image.network(
-                  "http://www.logospng.com/images/16/social-facebook-1489-1488-1494-1500-1497-1491-1505-1493-1499-1504-1493-1514-1508-1512-1505-1493-1501-1491-1497-1490-1497-1496-1500-1497-buzzlead-16424.png",
-                  color: Colors.white,
-                  width: 24.0,
-                  height: 24.0,
-                )):Container(width: 0.0,height: 0.0,),
+            isLoggedIn == null
+                ? CircularProgressIndicator()
+                : !isLoggedIn
+                    ? LoginButton("Continue with Facebook",
+                        color: Color(0xff3b5998),
+                        onPressed: initiateFacebookLogin,
+                        leading: Image.network(
+                          "http://www.logospng.com/images/16/social-facebook-1489-1488-1494-1500-1497-1491-1505-1493-1499-1504-1493-1514-1508-1512-1505-1493-1501-1491-1497-1490-1497-1496-1500-1497-buzzlead-16424.png",
+                          color: Colors.white,
+                          width: 24.0,
+                          height: 24.0,
+                        ))
+                    : Container(
+                        width: 0.0,
+                        height: 0.0,
+                      ),
             Padding(
-              padding: const EdgeInsets.only(top:20.0),
-              child: Text("By continuing, You agree to our Private Policy.",style: TextStyle(color: Colors.black45,fontSize: 14.0,fontWeight: FontWeight.w500),textAlign: TextAlign.center,),
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text(
+                "By continuing, You agree to our Private Policy.",
+                style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
             )
           ],
         ),
       ),
     );
   }
+
   void initiateFacebookLogin() async {
     var facebookLogin = FacebookLogin();
-    var result =
-    await facebookLogin.logInWithReadPermissions(['email','public_profile']);
+    var result = await facebookLogin
+        .logInWithReadPermissions(['email', 'public_profile']);
     switch (result.status) {
       case FacebookLoginStatus.error:
         print("Error");
@@ -83,49 +102,50 @@ class _SplashScreenState extends State<SplashScreen> {
         break;
       case FacebookLoginStatus.loggedIn:
         final token = result.accessToken.token;
-        final AuthCredential credential=FacebookAuthProvider.getCredential(accessToken: token);
+        final AuthCredential credential =
+            FacebookAuthProvider.getCredential(accessToken: token);
         print("Success");
-        final AuthResult authResult = await FirebaseAuth.instance.signInWithCredential(credential);
-        FirebaseUser user=authResult.user;
+        final AuthResult authResult =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+        FirebaseUser user = authResult.user;
         print("User $user");
         setState(() {
-          isLoggedIn=true;
+          isLoggedIn = true;
         });
         await UserController.updateUser(true);
-        if(user!=null){
+        if (user != null) {
           Navigator.of(context).pushNamed('/main');
         }
         break;
     }
   }
-
 }
+
 class LifecycleEventHandler extends WidgetsBindingObserver {
   LifecycleEventHandler({this.resumeCallBack, this.suspendingCallBack});
 
   final Function resumeCallBack;
   final Function suspendingCallBack;
 
-//  @override
-//  Future<bool> didPopRoute()
-
-//  @override
-//  void didHaveMemoryPressure()
-
   @override
-  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
     switch (state) {
       case AppLifecycleState.inactive:
       case AppLifecycleState.paused:
-      case AppLifecycleState.suspending:
+      case AppLifecycleState.detached:
         await suspendingCallBack();
         break;
       case AppLifecycleState.resumed:
         await resumeCallBack();
         break;
     }
-
   }
+
+//  @override
+//  Future<bool> didPopRoute()
+
+//  @override
+//  void didHaveMemoryPressure()
 
 //  @override
 //  void didChangeLocale(Locale locale)
